3fc1fe784b87fc90cda26df13b9f125ae36a0721
ghoulru <info@ghoul.ru>
2015-06-01 20:46:35 +0300

    t3

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ var Test = {
 	container: '',
 	init : function( _container ){
 		this.container = _container;
-		document.addEventListener('deviceready', Test.onDeviceReady, false);
+		this.bindEvents();
+	},
+	bindEvents: function(){
+		this.log(this.container);
+		document.addEventListener('deviceready', this.onDeviceReady, false);
 		//this.onDeviceReady();
+		this.log(typeof(Test.onDeviceReady));
+		this.log('this ready='+ typeof(this.onDeviceReady));
+
 	},
 	
 	onDeviceReady: function(){
+		this.log('onDeviceReady work');
 		document.querySelector(this.container).innerHTML = 'device ready';
+	},
+	
+	
+	log: function( str ){
+		document.querySelector('.log').innerHTML += str + '<br />';
 	}
 }
