78101e9f81e5ef248b85ca41e235d2736b5f0dc2
ghoulru <info@ghoul.ru>
2015-06-01 20:29:14 +0300

    test 1

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var Test {
+var Test = {
 	container: '',
 	init : function( _container ){
 		this.container = _container;
 		document.addEventListener('deviceready', this.onDeviceReady, false);
+		//this.onDeviceReady();
 	},
 	
 	onDeviceReady: function(){
-		document.querySelector(this.cont).innerHTML = 'device ready';
+		document.querySelector(this.container).innerHTML = 'device ready';
 	}
 }
